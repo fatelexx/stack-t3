e430d971797aa4485a38c26a9a7ac22ce4c293cc
fatelexx <fatelexx@outlook.com>
2024-03-26 21:04:03 +0800

    chore: remove old procedure

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -17,16 +17,12 @@ export const postRouter = createTRPCRouter({
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
+      // return ctx.db.customers.create({
+      //   data: {},
+      // });
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+    return null;
   }),
 });
