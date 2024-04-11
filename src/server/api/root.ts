368dc8fda0c56468a1c357453afdb8d5cf99065f
fatelexx <fatelexx@outlook.com>
2024-04-11 19:21:18 +0800

    chore: remove post router and module

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { dashboardRouter } from "./routers/dashboard";
 import { invoiceRouter } from "./routers/invoice";
@@ -9,7 +8,6 @@ import { invoiceRouter } from "./routers/invoice";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   dashboard: dashboardRouter,
   invoice: invoiceRouter,
 });
